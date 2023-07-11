21457ff1a058ad090d9847f8b1a96ab9c4c3558b
MARTIN Denis <denis@irlmobile.com>
2023-07-11 20:07:56 +0200

    Fixed macOS Build

--- Sources/SwiftUISnackbar/ColorExtension.swift
+++ Sources/SwiftUISnackbar/ColorExtension.swift
@@ -7,9 +7,16 @@ internal extension Color {
     var isDark: Bool {
         var r, g, b, a: CGFloat
         (r, g, b, a) = (0, 0, 0, 0)
+#if os(iOS)
         let uiColor = UIColor(self)
         uiColor.getRed(&r, green: &g, blue: &b, alpha: &a)
         let lum = 0.2126 * r + 0.7152 * g + 0.0722 * b
         return  lum < 0.50
+#else
+        let uiColor = NSColor(self)
+        uiColor.getRed(&r, green: &g, blue: &b, alpha: &a)
+        let lum = 0.2126 * r + 0.7152 * g + 0.0722 * b
+        return  lum < 0.50
+#endif
     }
 }
